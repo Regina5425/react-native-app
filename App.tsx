cb046dd8733fbb2957397b668118dd9f89c2d956
Regina <reina5425@gmail.com>
2025-02-23 18:01:11 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Some awesome text!</Text>
+      </View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="You can type in me"
+      />
+      <Button
+  title="Learn More"
+  color="#841584"
+/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
