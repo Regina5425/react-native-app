d99542b37a10323be6dc0cc4656d01050576aad1
Regina <reina5425@gmail.com>
2025-03-01 16:10:57 +0300

    base styles

--- App.tsx
+++ App.tsx
@@ -1,35 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 
 export default function App() {
+  // const width = Dimensions.get('window').width;
+  // width: (width / 2) - 5, где 5  - gap (условно если gap = 10, то между двумя элементами 10 / 2)
+  // const {height, width} = useWindowDimensions();
+
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Some awesome text!</Text>
+      <View style={styles.top}>
+        <Text style={styles.textStyle}>Hello Android!</Text>
+        <Button
+          title="Learn More"
+          color="#841584"
+        />
       </View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-        defaultValue="You can type in me"
-      />
-      <Button
-  title="Learn More"
-  color="#841584"
-/>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 70,
   },
+  textStyle: {color: 'green'},
+  top: {
+    flexDirection: 'row'
+  }
 });
