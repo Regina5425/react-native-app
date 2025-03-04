88af5ee7a9c0f639f61e3cf7d0a4ee483a4f82fd
Regina <reina5425@gmail.com>
2025-03-04 20:31:56 +0300

    animation

--- shared/Button/Button.tsx
+++ shared/Button/Button.tsx
@@ -1,12 +1,26 @@
-import { Pressable, PressableProps, StyleSheet, Text, View } from "react-native";
+import { Animated, Pressable, PressableProps, StyleSheet, Text, View } from "react-native";
 import { Colors, Fonts, Radius } from "../tokens";
 
 export function Button ({text, ...props}: PressableProps & {text: string}) {
+	const animatedValue = new Animated.ValueXY({x: 0, y: 0});
+
+	Animated.timing(animatedValue, {
+		toValue: {
+			x: 100,
+			y: 100
+		},
+		duration: 2000,
+		useNativeDriver: true
+	}).start()
+
   return (
     <Pressable {...props}>
-      <View style={styles.button}>
+      <Animated.View style={{...styles.button, transform: [
+				{translateX: animatedValue.x},
+				{translateY: animatedValue.y}
+			]}}>
         <Text style={styles.text}>{text}</Text>
-      </View>
+      </Animated.View>
     </Pressable>
   )
 }
