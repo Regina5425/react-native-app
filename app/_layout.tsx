0167c7921d8022d750d4b32259d09f70829dbd63
Regina <reina5425@gmail.com>
2025-03-08 20:35:11 +0300

    stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,23 @@
-import { Navigator, Slot } from 'expo-router';
-import { Text } from 'react-native';
+import { Stack } from 'expo-router';
+import { Colors } from '../shared/tokens';
 
 export default function RootLayout() {
 	return (
-		<Navigator>
-			<Text>Header</Text>
-			<Slot />
-			<Text>Footer</Text>
-		</Navigator>
+		<Stack
+			screenOptions={{
+				statusBarBackgroundColor: Colors.black,
+				contentStyle: {
+					backgroundColor: Colors.black,
+				},
+			}}
+		>
+			<Stack.Screen name="index" />
+			<Stack.Screen
+				name="restore"
+				options={{
+					presentation: 'modal',
+				}}
+			/>
+		</Stack>
 	);
 }
--- app/restore.tsx
+++ app/restore.tsx
@@ -1,10 +1,14 @@
-import { Link } from 'expo-router';
-import { Text } from 'react-native';
+import { Link, Stack } from 'expo-router';
+import { Text, View } from 'react-native';
+import { Colors } from '../shared/tokens';
 
 export default function Restore() {
 	return (
-		<Link href="/">
-			<Text>retstore page</Text>
-		</Link>
+		<View>
+			<Stack.Screen options={{ title: 'Восстановить пароль', statusBarBackgroundColor: Colors.black }} />
+			<Link href="/">
+				<Text>retstore page</Text>
+			</Link>
+		</View>
 	);
 }
