0ed4e4c96fb28b949bb3eba3a80a66809181d96c
Regina <reina5425@gmail.com>
2025-03-16 20:45:12 +0300

    image picker and permission

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,9 +1,44 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Text, View } from 'react-native';
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
+import { Button } from '../../shared/Button/Button';
 
 export default function Profile() {
+	const [image, setImage] = useState(null);
+	const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+
+	const verifyCameraPermissions = async () => {
+		if (cameraPermission?.status === PermissionStatus.UNDETERMINED) {
+			const res = await requestCameraPermission();
+			return res.granted;
+		}
+		if (cameraPermission?.status === PermissionStatus.DENIED) {
+			Alert.alert('Недостаточно прав для доступа к камере');
+			return false;
+		}
+		return true;
+	};
+
+	const pickAvatar = async () => {
+		const isPermissionGranted = await verifyCameraPermissions();
+
+		if (!isPermissionGranted) {
+			return;
+		}
+
+		const result = await launchCameraAsync({
+			mediaTypes: ['images'],
+			allowsEditing: true,
+			aspect: [1, 1],
+			quality: 0.5,
+		});
+		console.log(result);
+	};
+
 	return (
 		<View>
 			<Text>profile</Text>
+			<Button text="Выбрать изображение" onPress={pickAvatar} />
 		</View>
 	);
 }
