70404c075b1fd9f5b3a5f5411cfa47c737126a3a
Regina <reina5425@gmail.com>
2025-03-16 21:01:27 +0300

    pick with image

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { Alert, Text, View } from 'react-native';
-import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
+import { Alert, Image, Text, View } from 'react-native';
+import { launchCameraAsync, useCameraPermissions, PermissionStatus, launchImageLibraryAsync, useMediaLibraryPermissions } from 'expo-image-picker';
 import { Button } from '../../shared/Button/Button';
 
 export default function Profile() {
-	const [image, setImage] = useState(null);
+	const [image, setImage] = useState<string | null>(null);
 	const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+	const [libraryPermission, requestLibraryPermission] = useMediaLibraryPermissions();
 
 	const verifyCameraPermissions = async () => {
 		if (cameraPermission?.status === PermissionStatus.UNDETERMINED) {
@@ -19,7 +20,19 @@ export default function Profile() {
 		return true;
 	};
 
-	const pickAvatar = async () => {
+	const verifyLibraryPermissions = async () => {
+		if (libraryPermission?.status === PermissionStatus.UNDETERMINED) {
+			const res = await requestLibraryPermission();
+			return res.granted;
+		}
+		if (libraryPermission?.status === PermissionStatus.DENIED) {
+			Alert.alert('Недостаточно прав для доступа к камере');
+			return false;
+		}
+		return true;
+	};
+
+	const pickCameraAvatar = async () => {
 		const isPermissionGranted = await verifyCameraPermissions();
 
 		if (!isPermissionGranted) {
@@ -32,13 +45,42 @@ export default function Profile() {
 			aspect: [1, 1],
 			quality: 0.5,
 		});
-		console.log(result);
+		return result;
+	};
+
+	const pickImageAvatar = async () => {
+		const isPermissionGranted = await verifyLibraryPermissions();
+
+		if (!isPermissionGranted) {
+			return;
+		}
+
+		const result = await launchImageLibraryAsync({
+			mediaTypes: ['images'],
+			allowsEditing: true,
+			aspect: [1, 1],
+			quality: 0.5,
+		});
+		if (!result.assets) {
+			return;
+		}
+		setImage(result.assets[0].uri);
 	};
 
 	return (
 		<View>
 			<Text>profile</Text>
-			<Button text="Выбрать изображение" onPress={pickAvatar} />
+			<Button text="Снять изображение" onPress={pickCameraAvatar} />
+			<Button text="Выбрать изображение" onPress={pickImageAvatar} />
+			{image && (
+				<Image
+					source={{
+						uri: image,
+						width: 100,
+						height: 100,
+					}}
+				/>
+			)}
 		</View>
 	);
 }
